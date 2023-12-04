168270995d11faf31257b6c6ab23be947bf40cc0
xxziiko <xxziiko@gmail.com>
2023-12-05 00:44:29 +0900

    add: svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: { styledComponents: true },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+  images: {
+    domains: ['i.scdn.co'],
+  },
 }
 
 module.exports = nextConfig
