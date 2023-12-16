8367c1e6e2e24808ab14d4b7fed61c8c2c4e9d3e
xxziiko <xxziiko@gmail.com>
2023-12-16 13:40:54 +0900

    feat: api 추가

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,7 +1,26 @@
-import { callGetTodayHot100 } from '@/api/apis'
+import {
+  callGetTodayHot100,
+  callPostLoginApi,
+  callGetPlaylistApi,
+} from '@/api/apis'
 
 export const getTodayHot100 = () => {
   return callGetTodayHot100()
+    .then((response) => response.data)
+    .catch((error) => error)
+}
+
+export const sendPostLogin = (body) => {
+  return callPostLoginApi(body)
+    .then(
+      (response) => response.data,
+      (error) => console.log('error', error.data),
+    )
+    .catch((error) => error)
+}
+
+export const getPlaylist = (id) => {
+  return callGetPlaylistApi(id)
     .then((response) => response.data)
     .catch((error) => error)
 }
