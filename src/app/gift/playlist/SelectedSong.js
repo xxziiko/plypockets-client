8fef7ff8d626d876394a2578f3b34fa4909571d9
zu21un <jwjoo03@gmail.com>
2023-12-18 17:17:17 +0900

    feat: music search result page

---
+++ src/app/gift/playlist/SelectedSong.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+import { Typography } from '@/components'
+
+import { DefaultButton } from '@/components'
+
+const SelectedSong = ({ data }) => {
+  const { spotifyId, artistName, title, albumName, imageUrl, previewUrl } = data
+
+  const handleClickedNext = () => {
+    // TODO
+  }
+
+  return (
+    <Container>
+      <BackgroundImage imageUrl={imageUrl} />
+      <AlbumImage src={imageUrl} />
+      <Typography color={'#111'} fontSize={'14px'} weight={500} spacing={-0.56}>
+        {title}
+      </Typography>
+
+      <Typography
+        color={'#323232'}
+        fontSize={'12px'}
+        weight={500}
+        spacing={-0.48}
+      >
+        {artistName}
+      </Typography>
+
+      <audio controls autoplay name="media">
+        <source src={previewUrl} type="audio/mpeg" />
+      </audio>
+
+      <Typography color={'#111'} fontSize={'16px'} weight={700} spacing={-0.64}>
+        {albumName}
+      </Typography>
+
+      <ButtonWrapper>
+        <DefaultButton
+          command="다음으로"
+          color="#fff"
+          backgroundColor={({ theme }) => theme.colors.green}
+          onClick={() => handleClickedNext()}
+        />
+      </ButtonWrapper>
+    </Container>
+  )
+}
+
+export default SelectedSong
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 16px;
+  margin-top: 30px;
+`
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  width: 300px;
+  height: 326px;
+
+  background-image: url(${(props) => props.imageUrl});
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  opacity: 0.5;
+  filter: blur(20px);
+  -webkit-filter: blur(20px);
+  border-radius: 8px;
+
+  z-index: -100;
+`
+
+const AlbumImage = styled.img`
+  width: 144px;
+  height: 144px;
+  border-radius: 8px;
+`
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 48px;
+  display: flex;
+`
